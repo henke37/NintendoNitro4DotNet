99cb48138d08328d16c8790d09fb90bf37447102
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-05-10 19:45:58 +0200

    Add project files.

---
+++ NitroComposer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitroComposer
+{
+    public class Class1
+    {
+    }
+}
