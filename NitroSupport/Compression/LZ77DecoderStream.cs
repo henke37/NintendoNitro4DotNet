a9ba792f2add8f6d685b12185512d1f26dcc3446
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-07-13 04:50:46 +0200

    Decode back references

--- NitroSupport/Compression/LZ77DecoderStream.cs
+++ NitroSupport/Compression/LZ77DecoderStream.cs
@@ -6,16 +6,61 @@ namespace Nitro.Compression {
 		private int DecompressedLength;
 		private bool longLengths;
 
+		private BinaryReader Reader;
+
+		private int RunLength;
+		private int RunDistance;
+
 		public LZ77DecoderStream(Stream baseStream, int DecompressedLength, byte variant) {
 			this.baseStream = baseStream;
 			this.DecompressedLength = DecompressedLength;
-			this.longLengths = variant!=0;
+			this.longLengths = variant != 0;
 		}
 
 		public override int Read(byte[] buffer, int offset, int count) {
 			throw new System.NotImplementedException();
 		}
 
+		private void ReadReference() {
+			if(longLengths) {
+				byte x = Reader.ReadByte();
+				switch(x >> 4) {
+					case 0:
+						RunLength = x << 4;
+						x = Reader.ReadByte();
+						RunLength |= x >> 4;
+						RunLength += 0x11;
+
+						RunDistance = (x & 0x0F) << 8;
+						RunDistance |= Reader.ReadByte();
+						break;
+
+					case 1:
+						RunLength = (x & 0x0F) << 8;
+						x = Reader.ReadByte();
+						RunLength |= x << 4;
+						x = Reader.ReadByte();
+						RunLength |= x >> 4;
+						RunLength += 0x111;
+
+						RunDistance = (x & 0x0F) << 8;
+						RunDistance |= Reader.ReadByte();
+						break;
+
+					default:
+						RunLength = (x >> 4) + 1;
+						RunDistance = (x & 0x0F) << 8;
+						RunDistance |= Reader.ReadByte();
+						break;
+				}
+			} else {
+				byte x = Reader.ReadByte();
+				RunLength = x >> 4;
+				RunLength += 3;
+				RunDistance = (x & 0x0F) << 8;
+				RunDistance |= Reader.ReadByte();
+			}
+		}
 
 		#region Contract cruff
 
@@ -27,8 +72,10 @@ namespace Nitro.Compression {
 
 		public override long Length => DecompressedLength;
 
-		public override long Position { get => throw new System.NotImplementedException();
-			set => throw new System.NotImplementedException(); }
+		public override long Position {
+			get => throw new System.NotImplementedException();
+			set => throw new System.NotImplementedException();
+		}
 
 		public override void Flush() {
 			throw new System.NotSupportedException();
