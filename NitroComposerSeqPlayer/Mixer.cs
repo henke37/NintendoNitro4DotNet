c52e859f1a7996aa0471d11a20afbd4b440cdb81
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-08-27 20:38:23 +0200

    Initialize the mixer channels to silence that warning.

--- NitroComposerSeqPlayer/Mixer.cs
+++ NitroComposerSeqPlayer/Mixer.cs
@@ -3,5 +3,13 @@
 
 		internal MixerChannel[] channels;
 
+		private const int ChannelCount = 16;
+
+		public Mixer() {
+			channels = new MixerChannel[ChannelCount];
+			for(int channelIndex=0;channelIndex<ChannelCount;++channelIndex) {
+				channels[channelIndex] = new MixerChannel();
+			}
+		}
 	}
 }
