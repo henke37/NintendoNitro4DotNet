0e9b8c8553653c91df4b98eadaeb04d1dc743ae2
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-09-20 17:50:25 +0200

    Tilemap support

---
+++ NitroGraphics/NSCR.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Nitro.Graphics {
+	public class NSCR : Tilemap {
+
+		public NSCR() { }
+
+		public NSCR(Stream stream) {
+			var sections = new SectionedFile(stream);
+			if(sections.MainId != "RCSN") throw new InvalidDataException("Bad signature");
+
+			using(var r=new BinaryReader(sections.Open("NRCS"))) {
+				int WidthPixels = r.ReadUInt16();
+				int HeightPixel = r.ReadUInt16();
+
+				if((WidthPixels % Tile.Width) != 0) throw new InvalidDataException("Width not an even multipiel");
+				if((WidthPixels % Tile.Height) != 0) throw new InvalidDataException("Height not an even multipiel");
+
+				var colorMode = (ColorMode)r.ReadUInt16();
+				Format = (TileMapFormat)r.ReadUInt16();
+
+				var WidthTiles = WidthPixels / Tile.Width;
+				var HeightTiles = HeightPixel / Tile.Height;
+
+				TileMap = new TilemapEntry[HeightTiles, WidthTiles];
+
+				var dataSize = r.ReadUInt32();
+				var dataPos = r.ReadUInt32();
+				r.BaseStream.Position = dataPos;
+				LoadMap(r);
+			}
+		}
+
+		enum ColorMode {
+			CM_16x16,
+			CM_256x1,
+			CM_256x16
+		}
+	}
+}
---
+++ NitroGraphics/Tilemap.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace Nitro.Graphics {
+	public class Tilemap {
+
+		public TilemapEntry[,] TileMap;
+
+		protected TileMapFormat Format;
+
+		protected enum TileMapFormat {
+			Text,
+			Affine,
+			AffineExt
+		}
+
+		protected void LoadMap(BinaryReader reader) {
+			switch(Format) {
+				case TileMapFormat.Text:
+					LoadTextMap(reader);
+					break;
+				case TileMapFormat.Affine:
+					LoadAffineMap(reader);
+					break;
+				default:
+					throw new NotImplementedException();
+			}
+		}
+
+		protected void LoadTextMap(BinaryReader reader) {
+			for(var y = 0; y < TileMap.GetLength(0); ++y) {
+				for(var x = 0; x < TileMap.GetLength(0); ++x) {
+					var tile = new TilemapEntry();
+					var v = reader.ReadUInt16();
+					tile.TileId = v & 0x3FF;
+					tile.XFlip = (v & 0x400)!=0;
+					tile.YFlip = (v & 0x800)!=0;
+					tile.Palette = v >> 12;
+					TileMap[y, x] = tile;
+				}
+			}
+		}
+
+
+		protected void LoadAffineMap(BinaryReader reader) {
+			for(var y = 0; y < TileMap.GetLength(0); ++y) {
+				for(var x = 0; x < TileMap.GetLength(0); ++x) {
+					var tile = new TilemapEntry();
+					tile.TileId = reader.ReadByte();
+					TileMap[y, x] = tile;
+				}
+			}
+		}
+
+		public struct TilemapEntry {
+			public int TileId;
+			public bool XFlip;
+			public bool YFlip;
+			public int Palette;
+		}
+	}
+}
