061fc70078bc36b085ad84cdb325965cdfcbb56c
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-10-24 23:37:12 +0200

    Banner icon reading code to silence the unused var warnings.

--- NitroGraphics/Extensions.cs
+++ NitroGraphics/Extensions.cs
@@ -18,5 +18,21 @@ namespace Nitro.Graphics {
 			o.Load(reader, tileIndexShift, tileIndexNudge);
 			return o;
 		}
+
+		public static void Icon(this Banner banner) {
+			Tile[] tiles = new Tile[4];
+			using(var r = new BinaryReader(new MemoryStream(banner.IconPixels))) {
+				for(int tileIndex = 0; tileIndex < 4; ++tileIndex) {
+					tiles[tileIndex] = new Tile(r, TextureFormat.PLTT16);
+				}
+			}
+
+			BGR555[] palette=new BGR555[16];
+			using(var r=new BinaryReader(new MemoryStream(banner.IconPalette))) {
+				for(int colorIndex=0;colorIndex<16;++colorIndex) {
+					palette[colorIndex] = r.ReadBGR555();
+				}
+			}
+		}
 	}
 }
