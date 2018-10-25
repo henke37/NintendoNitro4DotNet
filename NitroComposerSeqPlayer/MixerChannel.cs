9a3a50ec823ac611897a63d8d142915a0e314f61
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-10-25 02:07:29 +0200

    Initial pulse and noise generator implementations.

--- NitroComposerSeqPlayer/MixerChannel.cs
+++ NitroComposerSeqPlayer/MixerChannel.cs
@@ -5,6 +5,23 @@ namespace NitroComposerSeqPlayer {
 
 		MixerChannelMode Mode = MixerChannelMode.Off;
 
+		private ushort noiseState;
+
+		internal int pulseWidth {
+			set {
+				currentPulseWidthTable = pulseWidthLUT[value];
+			}
+			get {
+				for(int i=0; i<8;++i) {
+					if(currentPulseWidthTable == pulseWidthLUT[i]) return i;
+				}
+				return -1;
+			}
+		}
+
+		private int[] currentPulseWidthTable;
+		private int pulseCounter;
+
 		public enum MixerChannelMode {
 			Off,
 			Pcm,
@@ -12,14 +29,51 @@ namespace NitroComposerSeqPlayer {
 			Noise
 		}
 
-		public void GenerateSamples() {
+		public void GenerateSamples(int sampleCount) {
 			switch(Mode) {
 				case MixerChannelMode.Off:
+					return;
 				case MixerChannelMode.Pcm:
+					GeneratePCM();
+					return;
+
 				case MixerChannelMode.Pulse:
+					GeneratePulse();
+					return;
+
 				case MixerChannelMode.Noise:
-					throw new NotImplementedException();
+					GenerateNoise();
+					return;
 			}
 		}
+
+		private int GeneratePCM() {
+			throw new NotImplementedException();
+		}
+
+		private int GeneratePulse() {
+			return currentPulseWidthTable[pulseCounter++ % 8];
+		}
+
+		private int GenerateNoise() {
+			if((noiseState &1)==1) {
+				noiseState = (ushort)((noiseState >> 1) ^ 0x6000);
+				return -0x7FFF;
+			} else {
+				noiseState >>= 1;
+				return 0x7FFF;
+			}
+		}
+
+		private static int[][] pulseWidthLUT = new int[][] {
+			new int[] {-0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF,  0x7FFF},
+			new int[] {-0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF,  0x7FFF,  0x7FFF},
+			new int[] {-0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF},
+			new int[] {-0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF},
+			new int[] {-0x7FFF, -0x7FFF, -0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF},
+			new int[] {-0x7FFF, -0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF},
+			new int[] {-0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF,  0x7FFF},
+			new int[] {-0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF, -0x7FFF}
+		};
 	}
 }
