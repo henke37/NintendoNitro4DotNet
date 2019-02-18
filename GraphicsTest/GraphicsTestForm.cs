b249fa84a5dc388fd6be5f95c98e6d0dfe28b567
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-02-19 00:49:39 +0100

    Another test project

---
+++ GraphicsTest/GraphicsTestForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsTest {
+	public partial class GraphicsTestForm : Form {
+		public GraphicsTestForm() {
+			InitializeComponent();
+		}
+	}
+}
