a596040a3d8817cb95b1756f73527aa6ace36a3d
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-02-20 20:07:51 +0100

    Test project for GK2 archives.

---
+++ GK2Test/TestForm.cs
@@ -0,0 +1,16 @@
+﻿using AceAttorney.GK2;
+using Nitro;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GK2Test {
+	public partial class TestForm : Form {
+
+		private NDS nds;
+
+		public TestForm(string[] args) {
+			nds = new NDS(File.OpenRead(args[0]));
+			MainArchive mainArchive=new MainArchive(nds.FileSystem.OpenFile(args[1]));
+		}
+	}
+}
